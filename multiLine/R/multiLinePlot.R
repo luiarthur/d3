41f0ea8e3dc9c600c5debf6463f7bc8f157e2c36
luiarthur <luiarthur@yahoo.com>
2015-05-11 13:54:12 -0600

    yes

--- multiLine/R/multiLinePlot.R
+++ multiLine/R/multiLinePlot.R
@@ -3,6 +3,9 @@ multiLinePlot <- function(xY,linkToTemplate=NULL) {
   # The remaining columns are the different y vectors.
   # The function currently only works properly if the x column is named
   # and the Y columns are all uniquely named
+  if (any(colnames(xY)=="")) {
+    colnames(xY) <- c("x",paste0("y",1:ncol(xY[,-1])))
+  }
   system("mkdir -p myMultiLinePlot")
   if (is.null(linkToTemplate)) {
     system("wget http://luiarthur.github.io/multiLineTemplate.html -O myMultiLinePlot/index.html")
@@ -21,15 +24,15 @@ multiLinePlot <- function(xY,linkToTemplate=NULL) {
 }
 
 # Example:
-#set.seed(5)
-#k <- 10
-#n <- 100
-#delta <- 20/k
-#x <- 1:n
-#y <- matrix(0,n,k)
-#y[,1] <- rnorm(n,30,sd=10)
-#for (j in 2:k) y[,j] <- delta + y[,j-1]
-#out <- cbind(x,y)
-#colnames(out) <- c("x",paste0("y",1:k))
-#
-#multiLinePlot(out)
+set.seed(5)
+k <- 10
+n <- 100
+delta <- 20/k
+x <- 1:n
+y <- matrix(0,n,k)
+y[,1] <- rnorm(n,30,sd=10)
+for (j in 2:k) y[,j] <- delta + y[,j-1]
+out <- cbind(x,y)
+colnames(out) <- c("x",paste0("y",1:k))
+
+multiLinePlot(out)
