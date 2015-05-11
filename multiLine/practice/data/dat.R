58b210fe5065960274abf4d50d27470a944ed32f
luiarthur <luiarthur@yahoo.com>
2015-05-11 11:04:41 -0600

    yes

--- multiLine/practice/data/dat.R
+++ multiLine/practice/data/dat.R
@@ -1,13 +1,15 @@
-set.seed(6)
+set.seed(5)
 k <- 10
-n <- 1000
+n <- 100
+delta <- 20/k
 
 x <- 1:n
-y <- apply(matrix(1:k),1,function(dummy) rnorm(n,runif(n,30,70),sd=10))
+y <- matrix(0,n,k)
+y[,1] <- rnorm(n,30,sd=10)
+for (j in 2:k) y[,j] <- delta + y[,j-1]
+#y <- apply(matrix(1:k),1,function(dummy) rnorm(n,runif(n,30,70),sd=10))
 
 #plot(x,y[,3])
 out <- cbind(x,y)
 colnames(out) <- c("x",paste0("y",1:k))
 write.table(out,file="sim1.tsv",quote=F,row.names=F,sep="\t")
-
-			
